e3d8ddaeee4bc66200a28d5a8bd6d2f8fadcb647
Alexey Plenkin <aveplen@yandex.ru>
2022-05-31 03:36:05 +0300

    cheezy but works

--- grpc.py
+++ grpc.py
@@ -1,3 +1,5 @@
+import os
+
 import grpc
 import face_recognition
 import numpy as np
@@ -10,18 +12,22 @@ from .protos.s3file import s3file_pb2, s3file_pb2_grpc
 class Facerec(facerec_pb2_grpc.FaceRecognition):
 
     def __init__(self):
-        channel = grpc.insecure_channel('localhost:8083')
+        channel = grpc.insecure_channel('localhost:9090')
         self.stub = s3file_pb2_grpc.S3GatewayStub(channel)
 
     def ExtractFFVectorV1(self, request, context):
         print(f"=================\n{request}")
 
-        response = self.stub.Reply(s3file_pb2.GetImageObjectRequest(id=request.id))
-        imgBlob = response.contents
+        response = self.stub.GetImageObject(s3file_pb2.GetImageObjectRequest(id=request.id))
+        img_blob = response.contents
+        print(img_blob)
 
-        img = np.array(imgBlob)
-        img_face_encoding = face_recognition.face_encodings(img)[0]
-        print(img_face_encoding.shape)
-        print(img_face_encoding)
 
-        return facerec_pb2.ExtractFFVectorV1Response(ffvc=[1.1, 2.2, 3.3, 4.4])
+        if os.path.exists("app.cpp"):
+            os.remove("app.cpp")
+
+        with open('tmp', 'wb') as f:
+            f.write(img_blob)
+            img = face_recognition.load_image_file('tmp')
+            img_face_encoding = face_recognition.face_encodings(img)[0]
+            return facerec_pb2.ExtractFFVectorV1Response(ffvc=img_face_encoding)
