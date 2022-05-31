a176b96f789cb24b8d0cd497f62aad9e4b23ce22
Alexey Plenkin <aveplen@yandex.ru>
2022-05-31 03:57:50 +0300

    minor cleanup

--- grpc.py
+++ grpc.py
@@ -12,22 +12,18 @@ from .protos.s3file import s3file_pb2, s3file_pb2_grpc
 class Facerec(facerec_pb2_grpc.FaceRecognition):
 
     def __init__(self):
-        channel = grpc.insecure_channel('localhost:9090')
+        channel = grpc.insecure_channel("localhost:9090")
         self.stub = s3file_pb2_grpc.S3GatewayStub(channel)
 
     def ExtractFFVectorV1(self, request, context):
-        print(f"=================\n{request}")
-
         response = self.stub.GetImageObject(s3file_pb2.GetImageObjectRequest(id=request.id))
         img_blob = response.contents
-        print(img_blob)
 
+        if os.path.exists("tmp"):
+            os.remove("tmp")
 
-        if os.path.exists("app.cpp"):
-            os.remove("app.cpp")
-
-        with open('tmp', 'wb') as f:
+        with open("tmp", "wb") as f:
             f.write(img_blob)
-            img = face_recognition.load_image_file('tmp')
+            img = face_recognition.load_image_file("tmp")
             img_face_encoding = face_recognition.face_encodings(img)[0]
             return facerec_pb2.ExtractFFVectorV1Response(ffvc=img_face_encoding)
